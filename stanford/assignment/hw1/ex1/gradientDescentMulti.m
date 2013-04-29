68b8eaaaf30a433ba5a43f8803c0e1a597561cfe
Weinan Ma <mwn0418@gmail.com>
2013-04-28 18:45:37 -0700

    Multi-variable version for hw1

--- stanford/assignment/hw1/ex1/gradientDescentMulti.m
+++ stanford/assignment/hw1/ex1/gradientDescentMulti.m
@@ -16,16 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    n = length(theta);
+    theta_old = theta;
+    for j = 1:n
+        delta = 0
+        for i = 1:m
+            delta += ( X(i,:)*theta_old - y(i) ) * X(i,j);
+        end
+        theta(j) = theta_old(j) - alpha*delta/m;
+    end
 
     % ============================================================
 
