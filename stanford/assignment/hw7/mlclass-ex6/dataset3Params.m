3ed2272f395eca1a16d2637819c7ea22e0c54104
Weinan Ma <mwn0418@gmail.com>
2013-11-07 22:34:13 -0800

    Create ex6_C_sigma.m for testing my C/sigma.
    Finish the init version of C/sigma loop, ready for multiple value run!

--- stanford/assignment/hw7/mlclass-ex6/dataset3Params.m
+++ stanford/assignment/hw7/mlclass-ex6/dataset3Params.m
@@ -23,7 +23,39 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+i = 1;
+j = 1;
+err = 1000000000000;
+indexi = i;
+indexj = j;
 
+%C_l = [.001, .01, .1, 10],
+%sigma_l = [.003, .03, .3, 30],
+
+C_l = [.01, 10],
+sigma_l = [.03, 30],
+
+for i = 1:length(C_l),
+	for j = 1:length(sigma_l),
+		C = C_l(i),
+		sigma = sigma_l(j),
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		error_l(i,j) = mean(double(predictions ~= yval))
+		if error_l(i,j) < err,
+			err = error_l(i,j);
+			indexi = i;
+			indexj = j;
+		end
+		j++
+	end
+	i++;
+end
+
+error_l,
+
+C = C_l(indexi),
+sigma = sigma_l(indexj),
 
 
 
---
+++ stanford/assignment/hw7/mlclass-ex6/ex6_C_sigma.m
@@ -0,0 +1,45 @@
+%% Machine Learning Online Class
+%  Exercise 6 | Support Vector Machines
+%
+%  Instructions
+%  ------------
+% 
+%  This file contains code that helps you get started on the
+%  exercise. You will need to complete the following functions:
+%
+%     gaussianKernel.m
+%     dataset3Params.m
+%     processEmail.m
+%     emailFeatures.m
+%
+%  For this exercise, you will not need to change any code in this file,
+%  or any other files other than those mentioned above.
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
+
+%  This is a different dataset that you can use to experiment with. Try
+%  different values of C and sigma here.
+% 
+
+% Load from ex6data3: 
+% You will have X, y in your environment
+load('ex6data3.mat');
+
+fprintf('Loading and Visualizing Data ...\n')
+% Plot training data
+plotData(X, y);
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+
+% Train the SVM
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
