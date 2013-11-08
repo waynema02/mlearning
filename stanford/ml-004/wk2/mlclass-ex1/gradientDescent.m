7d7c413c09a59b2a200e40b7306f0cf2861e0b8e
Weinan Ma <mawn02@gmail.com>
2013-11-08 15:31:39 -0800

    Submitted the one variable gradient descent.

--- stanford/ml-004/wk2/mlclass-ex1/gradientDescent.m
+++ stanford/ml-004/wk2/mlclass-ex1/gradientDescent.m
@@ -16,12 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    theta_temp = theta;
+    theta(1) = theta_temp(1) - alpha * 1/m * X(:,1)' * (X*theta_temp - y);
+    theta(2) = theta_temp(2) - alpha * 1/m * X(:,2)' * (X*theta_temp - y);
+    computeCost(X, y, theta)
 
     % ============================================================
 
