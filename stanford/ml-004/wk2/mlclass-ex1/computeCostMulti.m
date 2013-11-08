50bcb240a8704413c0f603a292590d8ace018ed7
Weinan Ma <mawn02@gmail.com>
2013-11-08 15:53:58 -0800

    Submitted multi variable cost function.
    This is actually the SAME as the single variable I wrote.

--- stanford/ml-004/wk2/mlclass-ex1/computeCostMulti.m
+++ stanford/ml-004/wk2/mlclass-ex1/computeCostMulti.m
@@ -13,9 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+SS = (X*theta-y)' * (X*theta-y);
+J = SS/(2*m);
 
 % =========================================================================
 
